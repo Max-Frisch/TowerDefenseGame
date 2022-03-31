592ee13ec6a72645c7657962a0980fe8b62b8dfb
Max <max-frisch@gmx.net>
2022-03-31 18:46:59 +0700

    Update Game.java

--- main/Game.java
+++ main/Game.java
@@ -1,4 +1,15 @@
 package main;
 
 public class Game {
+
+    public Game() {
+        System.out.println("Hello from the constructor");
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello from main");
+
+        Game game = new Game();
+    }
 }
